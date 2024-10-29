684fced6a84cb770141b62e6624883d189780c70
Vishvjeet Singh Rathore <VishvjeetSingh523@gmail.com>
2024-10-29 16:02:50 +0530

    use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Image from "next/image";
 import useSocket from '../hooks/useSocket';
+
 export default function Home() {
   const socket = useSocket("yatodharm tato jayah");
   return (
